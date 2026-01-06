23d4f865d5df7bb28f7090b16ad2bf13a846fb6b
sart55 <105958020+sart55@users.noreply.github.com>
2026-01-07 01:38:13 +0530

    Add files via upload

---
+++ getmapping/GetmappingApplication.java
@@ -0,0 +1,13 @@
+package com.example.getmapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetmappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetmappingApplication.class, args);
+	}
+
+}
